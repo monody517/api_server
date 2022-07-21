f3cfff5a738014198d9b0cb6f7f159d35de59927
wenpei <2737716714@qq.com>
2022-07-21 11:13:14 +0800

    更新用户信息接口完成

--- router/userinfo.js
+++ router/userinfo.js
@@ -3,7 +3,13 @@ const express = require('express')
 const router = express.Router()
 
 const userInfoHandle = require('../router_handler/userinfoHandle')
+const updateUserInfo = require('../router_handler/userinfoHandle')
 
-router.get('/userinfo',userInfoHandle.getUserInfo)
+const expressJoi = require('@escook/express-joi')
+
+const {update_userinfo_schema} = require('../schema/user')
+
+router.get('/userinfo',userInfoHandle.getUserInfo) // 获取用户信息
+router.post('/userinfo',expressJoi(update_userinfo_schema),userInfoHandle.updateUserInfo) // 更新用户信息
 
 module.exports = router
--- schema/user.js
+++ schema/user.js
@@ -4,10 +4,23 @@ const joi = require('joi')
 const username = joi.string().alphanum().min(1).max(10).required()
 const password = joi.string().pattern(/^[\S]{6,12}$/).required()
 
+// 定义 id, nickname, emial 的验证规则
+const id = joi.number().integer().min(1).required()
+const nickname = joi.string().required()
+const email = joi.string().email().required()
+
 // 定义验证注册和登录表单数据规则对象
 exports.reg_login_schema = {
     body: {
         username,
         password
+    }
+}
+
+exports.update_userinfo_schema = {
+    body: {
+        id,
+        nickname,
+        email
     }
 }
