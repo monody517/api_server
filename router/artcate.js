b0bbbc37d619392163ea52a0a510b404e3034542
wenpei <2737716714@qq.com>
2022-07-21 15:11:31 +0800

    新增文章分类接口完成

--- router/artcate.js
+++ router/artcate.js
@@ -4,6 +4,11 @@ const router = express.Router()
 
 const artcate_handler = require('../router_handler/artcateHandle')
 
+const expressJoi = require('@escook/express-joi')
+
+const {add_cate_schema} = require('../schema/artcate')
+
 router.get('/cates',artcate_handler.getArticleCates)
+router.post('/addcates',expressJoi(add_cate_schema),artcate_handler.addArticleCates)
 
 module.exports = router
--- router_handler/artcateHandle.js
+++ router_handler/artcateHandle.js
@@ -13,3 +13,28 @@ exports.getArticleCates = (req,res)=>{
         })
     })
 }
+
+exports.addArticleCates = (req,res) => {
+    const sqlStr1= 'select * from ev_article_cate where name=? or alias=?'
+
+    db.query(sqlStr1,[req.body.name,req.body.alias],(err,results)=>{
+        if(err) return res.cc(err)
+
+          // 分类名称 和 分类别名 都被占用
+        if (results.length === 2) return res.cc('分类名称与别名被占用，请更换后重试！')
+        if (results.length === 1 && results[0].name === req.body.name && results[0].alias === req.body.alias) return res.cc('分类名称与别名被占用，请更换后重试！')
+        // 分类名称 或 分类别名 被占用
+        if (results.length === 1 && results[0].name === req.body.name) return res.cc('分类名称被占用，请更换后重试！')
+        if (results.length === 1 && results[0].alias === req.body.alias) return res.cc('分类别名被占用，请更换后重试！')
+
+        const sqlStr2 = 'insert into ev_article_cate set ?'
+
+        db.query(sqlStr2,req.body,(err,results)=>{
+            if(err) return res.cc(err)
+
+            if(results.affectedRows!==1) return res.cc('新增文章分类失败！')
+
+            res.cc(0,'新增文章分类成功！')
+        })
+    })
+}
